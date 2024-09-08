3ab230af6219b891df166fa4342299825c6e5373
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-09-08 09:12:34 +0300

    ProjectClient is created

---
+++ src/main/java/com/cydeo/client/ProjectClient.java
@@ -0,0 +1,18 @@
+package com.cydeo.client;
+
+import com.cydeo.dto.ProjectResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "project-service", url = "localhost:8082")
+public interface ProjectClient {
+
+    @GetMapping("/api/v1/project/check/{projectCode}")
+    ResponseEntity<ProjectResponse> checkByProjectCode(@PathVariable("projectCode") String projectCode);
+
+    @GetMapping("/api/v1/project/read/manager/{projectCode}")
+    ResponseEntity<ProjectResponse> getManagerByProject(@PathVariable("projectCode") String projectCode);
+
+}
